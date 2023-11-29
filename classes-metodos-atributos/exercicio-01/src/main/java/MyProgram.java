1fe003e3ed55d8af949d3756f9f930cf22d7310c
Kadu Pantoja <dsc.pantoja@gmail.com>
2023-11-28 21:10:43 -0300

    Ajustando o push do Rafael.

--- classes-metodos-atributos/exercicio-01/src/main/java/MyProgram.java
+++ classes-metodos-atributos/exercicio-01/src/main/java/MyProgram.java
@@ -1,34 +1,30 @@
 import java.util.Scanner;
 
 /**
-  * MyProgram
-  */
- public class MyProgram {
- 
- 
+ * MyProgram
+ */
+public class MyProgram {
+
     public static void main(String[] args) {
-        
-        Scanner in = new Scanner (System.in);
-        
+
+        Scanner in = new Scanner(System.in);
+
         double nota1, nota2;
-        
+
         System.out.println("Entre com a primeira nota");
         nota1 = in.nextDouble();
-        
+
         System.out.println("Entre com a segunda nota");
         nota2 = in.nextDouble();
 
+        double media = (nota1 + nota2) / 2;
 
-        double media = (nota1+nota2)/2;
-        
         if (media >= 7.0) {
             System.out.println("Voce foi Aprovado!");
         } else {
             System.out.println("Voce foi Reprovado!");
         }
-    in.close();
+        in.close();
     }
 
 }
-
-
